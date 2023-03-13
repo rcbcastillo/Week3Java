684907ddfb7ca3ec777efd677507968fe93eeaf4
rcbcastillo <rcbcastillo@gmail.com>
2023-03-13 11:38:14 +0000

    Add Flowchart2 code

--- src/FlowchartLoop1/FlowchartLoop1.java
+++ src/FlowchartLoop1/FlowchartLoop1.java
@@ -5,12 +5,29 @@ public class FlowchartLoop1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Flowchart1:");
-		int A = 100;
-		while ( A <= 200) {
-			System.out.println(A);
-			A++;
+		int A1 = 100;
+		while ( A1 <= 200) {
+			System.out.println(A1);
+			A1++;
 		}
 		
-	}
-
+		System.out.println("Flowchart2:");
+		int A2 = 100;
+		
+		while (A2 <= 200) {
+			if ( A2 % 2 == 0) {
+				System.out.println("-");
+				A2++;
+				
+				
+			}else {
+				System.out.println("*");
+				A2++;
+			}
+		}
+		
+	} 
+		
 }
+
+
