1381c97074326d09c980893a20ea6d4249bd99e6
rcbcastillo <rcbcastillo@gmail.com>
2023-03-13 11:54:13 +0000

    Add Print10Times method

--- src/FlowchartLoop1/FlowchartLoop1.java
+++ src/FlowchartLoop1/FlowchartLoop1.java
@@ -2,6 +2,16 @@ package FlowchartLoop1;
 
 public class FlowchartLoop1 {
 	
+	static void print10Times() {
+		System.out.println("Print 10X from 1-10");
+		for (int i = 1; i < 11; i++) {
+			System.out.println("This is the : " + i + "X" );
+			for (int j = 1; j <= 10; j++) {
+				System.out.println(j);
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Flowchart1:");
@@ -25,6 +35,8 @@ public class FlowchartLoop1 {
 				A2++;
 			}
 		}
+		
+		print10Times();
 		
 	} 
 		
