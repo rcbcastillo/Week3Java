0fc9b58ef80be5eec594996c2c76e649697ff849
rcbcastillo <rcbcastillo@gmail.com>
2023-03-13 11:24:23 +0000

    Add FlowchartLoops1 source file

---
+++ src/FlowchartLoop1/FlowchartLoop1.java
@@ -0,0 +1,16 @@
+package FlowchartLoop1;
+
+public class FlowchartLoop1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Flowchart1:");
+		int A = 100;
+		while ( A <= 200) {
+			System.out.println(A);
+			A++;
+		}
+		
+	}
+
+}
