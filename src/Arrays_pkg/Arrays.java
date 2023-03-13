c470cbc8df1e0096293b6712840c943e855569f6
rcbcastillo <rcbcastillo@gmail.com>
2023-03-13 15:23:34 +0000

    Add Exercise1 from Arrays

---
+++ src/Arrays_pkg/Arrays.java
@@ -0,0 +1,14 @@
+package Arrays_pkg;
+
+public class Arrays {
+	// Create an array that will hold 10 int values
+	static int arraysOfNums[] = {1,2,3,4,5,6,7,8,9,10};
+
+	public static void main(String[] args) {
+		for (int j=0; j < arraysOfNums.length; j++) {
+			 System.out.println(arraysOfNums[j] );
+		 }
+
+	}
+
+}
