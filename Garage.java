eb5457bfcf9eabc67f20c7d4ab157a5712cdcadc
rcbcastillo <rcbcastillo@gmail.com>
2023-03-16 14:32:32 +0000

    Add method that iterates through each Vehicle to calculate a bill

--- Garage.java
+++ Garage.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Garage {
 	
+	
 	// Create list to store vehicles
 	List<Vehicle> vehicles = new ArrayList<>();
 	
@@ -13,6 +14,32 @@ public class Garage {
 		return this.vehicles.add(anyVehicle);
 	}
 	
+	// Create a method to calculate Billing
 	
+	public void calculatePlateBilling() {
+		//String message = "Enter a valid category";
+		for (Vehicle item: this.vehicles) {
+			String currentItemClass = (item.getClass().getName());
+						
+			if (currentItemClass.endsWith("Car")) {
+				System.out.println("£54.87");
+			} else if (currentItemClass.endsWith("Motorbike")) {
+				System.out.println("£24.87");
+			} else if (currentItemClass.endsWith("Bicycle")) {
+			System.out.println("No charges");
+			}
+			
+		}
+	}
+	
+	
+	
+	/*Create a method in Garage that iterates through each Vehicle, 
+	calculating a bill for each type of Vehicle in a different way,
+	depending on the type of Vehicle it is 
+	(this does not need to be complex).*/
+	
+	
+
 
 }
